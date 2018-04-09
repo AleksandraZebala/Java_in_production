a2750668b80fd6e164c1f7130ac4862db6a935a8
Aleksandra Zębala <aleksandra.zebala95@gmail.com>
2018-04-09 20:04:44 +0200

    Loggers hell solution

--- week_3/zad_1/src/main/java/Main.java
+++ week_3/zad_1/src/main/java/Main.java
@@ -20,40 +20,45 @@ public class Main {
 
         logger.info("Program started with arguments: "+ Arrays.toString(args));
 
-        Options options = new Options();
-        options.addRequiredOption("price", null, true, "");
-        options.addRequiredOption("age", null, true, "");
-        options.addRequiredOption("clientId", null, true, "");
-        options.addRequiredOption("companyId", null, true, "");
-
-        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine cmd;
 
         try {
 
-            CommandLine cmd = commandLineParser.parse(options, args);
+            Options options = new Options();
+            options.addRequiredOption("price", null, true, "");
+            options.addRequiredOption("age", null, true, "");
+            options.addRequiredOption("clientId", null, true, "");
+            options.addRequiredOption("companyId", null, true, "");
 
-            BigDecimal price = BigDecimal.valueOf(Double.parseDouble(cmd.getOptionValue("price")));
-            Integer age = Integer.parseInt(cmd.getOptionValue("age"));
-            Integer clientId = Integer.parseInt(cmd.getOptionValue("clientId"));
-            Integer companyId = Integer.parseInt(cmd.getOptionValue("companyId"));
-
-            BigDecimal result = price.subtract(discountCalculator.calculateDiscount(price, age));
-
-            logger.info("Making payment transaction of " + result + " dollars from " + clientId + " to " + companyId);
-
-            if (paymentsService.makePayment(clientId, companyId, result)) {
-                logger.info("Transaction successfull!");
-            } else {
-                logger.info("Transaction denied!");
-            }
+            CommandLineParser commandLineParser = new DefaultParser();
+            cmd = commandLineParser.parse(options, args);
         }
 
         catch (MissingOptionException e) {
             logger.error("Missed arguments: " + e.getMissingOptions());
+            return;
         }
 
-        catch (ParseException e){
+        catch (Exception e){
             logger.error(e.getMessage());
+            return;
+        }
+
+        BigDecimal price = BigDecimal.valueOf(Double.parseDouble(cmd.getOptionValue("price")));
+        Integer age = Integer.parseInt(cmd.getOptionValue("age"));
+        Integer clientId = Integer.parseInt(cmd.getOptionValue("clientId"));
+        Integer companyId = Integer.parseInt(cmd.getOptionValue("companyId"));
+
+        BigDecimal result = price.subtract(discountCalculator.calculateDiscount(price, age));
+
+        logger.info("Making payment transaction of " + result + " dollars from " + clientId + " to " + companyId);
+
+        if (paymentsService.makePayment(clientId, companyId, result)) {
+            logger.info("Transaction successfull!");
+        }
+
+        else {
+            logger.info("Transaction denied!");
         }
     }
 }
