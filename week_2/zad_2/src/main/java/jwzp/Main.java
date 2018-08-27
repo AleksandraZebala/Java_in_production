43ddaf9b11688b63119a523960abd27f9c88f48b
Aleksandra Zębala <aleksandra.zebala95@gmail.com>
2018-08-27 18:10:54 +0200

    Added Dockerfile

--- week_2/zad_2/src/main/java/jwzp/Main.java
+++ week_2/zad_2/src/main/java/jwzp/Main.java
@@ -12,36 +12,56 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args){
-        ApplicationContext applicationContext = getApplicationContext(args);
 
+        if (args.length == 1 && args[0].equals("docker")) {
+            try {
+                Properties properties = new Properties();
+                InputStream inputStream = new FileInputStream("/storage/generator.properties");
+                properties.load(inputStream);
+                args = new String[]{
+                        "-customerIds", properties.getProperty("customerIds"),
+                        "-dateRange", properties.getProperty("dateRange").replace("\"", ""),
+                        "-itemsFile", "." + properties.getProperty("itemsFile"),
+                        "-itemsCount", properties.getProperty("itemsCount"),
+                        "-itemsQuantity", properties.getProperty("itemsQuantity"),
+                        "-eventsCount", properties.getProperty("eventsCount"),
+                        "-outDir", "." + properties.getProperty("outDir"),
+                        "--format=" + properties.getProperty("format") };
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ApplicationContext applicationContext = getApplicationContext(args);
         try {
+
             String info = "Program started with arguments:";
             for(String arg : args)
                 info += " " + arg;
             logger.info(info);
+
             CommandParser commandParser = (CommandParser) applicationContext.getBean("commandParser");
             Generator generator = (Generator) applicationContext.getBean("generator");
-
             args = Arrays.stream(args).filter(s -> !s.contains("--format")).toArray(String[]::new);
-
             InputData data = commandParser.parse(args);
             FileWrapper fileWrapper = new FileWrapper(data.getItemsFile());
             ArrayList<Transaction> transactions = generator.generate(data, fileWrapper);
-
             String saverFormat = applicationContext.getEnvironment().getProperty("format");
             if(saverFormat == null)
                 saverFormat = "json";
-
             OutputManager outputManager = (OutputManager) applicationContext.getBean(saverFormat);
-
             outputManager.saveToFiles(transactions, data.getOutDir());
 
             logger.info("Transactions generated succesfully");
--- week_2/zad_2/src/main/java/jwzp/module/OutputManager.java
+++ week_2/zad_2/src/main/java/jwzp/module/OutputManager.java
@@ -1,12 +1,8 @@
 package jwzp.module;
 
 import java.util.ArrayList;
-
 import jwzp.data.Transaction;
 
-/**
- * Created by Olusia on 2018-08-22.
- */
 public interface OutputManager {
     void saveToFiles(ArrayList<Transaction> transactions, String outDir) throws Exception;
 }
